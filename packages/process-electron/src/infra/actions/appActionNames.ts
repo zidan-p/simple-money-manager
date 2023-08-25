ab9b45de9414c2ddfc988fb898e41995102d2fc9
zidan-p <zidanputrarahman153@gmail.com>
2023-08-25 17:43:49 +0700

    moro close in bright spot

--- packages/process-electron/src/infra/actions/appActionNames.ts
+++ packages/process-electron/src/infra/actions/appActionNames.ts
@@ -9,5 +9,5 @@ export const CLOSE = "close";
 export const MINIMIZE = "minimize";
 export const MAXIMIZE = "maximaze";
 export const UNMAXIMIZE = "unmaximize";
-export const RESTORE_DOWN = "restore_down";
-export const OPEN_DIALOG_IMAGE_SELECTOR = "open_dialog_image_selector";
+export const RESTORE_DOWN = "restoreDown";
+export const OPEN_DIALOG_IMAGE_SELECTOR = "openDialogImageSelector";
